cbb751452d646c1e00dcba1c391c6a1eeab26a8f
Carmel <elisabethcarmela.appdev@gmail.com>
2024-04-29 15:29:27 +0700

    Initial Commit

---
+++ JogeDang/JogeDangApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogeDangApp.swift
+//  JogeDang
+//
+//  Created by Elisabeth Carmela on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JogeDangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
